db0f87ed7c94a573e6b638a0a42f9cb8defcf066
Mbonyimfura <mbonyimfuratresor@gmail.com>
2024-04-08 21:46:51 +0200

    Add models

---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        minLength: 6
+    },
+})
+
+UserSchema.pre('save', async function (next){
+    const user = this
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password,8)
+    }
+    next()
+})
+const User = mongoose.model('User',UserSchema)
+module.exports= User
